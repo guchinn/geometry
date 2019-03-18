20f44b25f03b31dff1b374e688d99ee4c0b86827
Сухарев Никита Андреевич <ip814s26@mwp05.csc.local>
2019-03-18 14:13:37 +0700

    kakah

--- geometry.c
+++ geometry.c
@@ -1,7 +1,18 @@
-#include <iostream>
+#include <stdio.h>
 #include <math.h>
+#include <stdio>
 using namespace std;
 int main()
 {
+float x1,y1,r1,x2,y2,r2,r;
+float dist;
+
+printf("circle #1:");
+scanf("%f%f%f",&x1,&y1,&r1);
+
+printf("\n\nCircle #2:");
+scanf("%f%f%f",&x2,&y2,&r2);
+
+dist= sqrt((x2-x1)*(x2-x1)+(
 string inputs[3];
 for
